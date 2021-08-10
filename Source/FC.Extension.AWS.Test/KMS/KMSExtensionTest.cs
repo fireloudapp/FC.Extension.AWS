2d10df7a658430ba1747fc0ad29c24e8fff2d710
Ganeshram Ravi <sr.ganeshram@gmail.com>
2021-08-10 14:53:24 +0530

    A simple way to get KMS Key, a reusable method.
    A simple way to get KMS Key, a reusable method.

---
+++ Source/FC.Extension.AWS.Test/KMS/KMSExtensionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FC.Extension.AWS.Test
+{
+    public class KMSExtensionTest
+    {
+        private readonly ITestOutputHelper _output;
+        public KMSExtensionTest(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
+        //[Fact]
+        public void GetKMSKey_Test()
+        {
+            string kmsMasterKey = "";
+            var keyInfo = kmsMasterKey.GetKMSKeyInfo(clientAccessKey: "",
+                clientSecretKey: "",
+                RegionEndpoint.USEast1
+                );
+            keyInfo.Result.CypherKey.ShouldBeEmpty();
+            _output.WriteLine($" Cypher Key : ", keyInfo.Result.CypherKey , $" Plain Key : " + keyInfo.Result.Plainkey);
+            _output.WriteLine(kmsMasterKey);
+        }
+    }
+}
